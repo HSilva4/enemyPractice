a7847f9512e19ef221436f1edd56dd23854d8eb7
HSilva4 <hsilva@uw.edu>
2016-01-28 13:23:03 -0800

    Added viewing circle collision with enemy

--- enemyPractice/public_html/main.js
+++ enemyPractice/public_html/main.js
@@ -101,7 +101,7 @@ ViewingCircle.prototype.collide = function (other) {
   var distanceC2 = Math.sqrt(((other.right - this.x) * (other.right - this.x)) + ((other.top - this.y) * (other.top - this.y)));
   var distanceC3 = Math.sqrt(((other.left - this.x) * (other.left - this.x)) + ((other.bottom - this.y) * (other.bottom - this.y)));
   var distanceC4 = Math.sqrt(((other.right - this.x) * (other.right - this.x)) + ((other.bottom - this.y) * (other.bottom - this.y)));
-  
+  console.log(distanceC1 + " " + distanceC2 + " " + distanceC3 + " " + distanceC4);
   if (distanceC1 < this.radius || distanceC2 < this.radius || distanceC3 < this.radius || distanceC4 < this.radius) {
     return true;
   }
